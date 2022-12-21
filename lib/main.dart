7aeb14df1ac1af0e2a005792548fbe99ed90a6b7
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-12-21 16:20:42 +0800

    class constructor added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'story_brain.dart';
+
+StoryBrain storyBrain = StoryBrain();
 
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
